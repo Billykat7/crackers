6fbc9bee2dbaa084bd430662834b0d9a9e3b151f
Billy Katalayi <billy@sbn.systems>
2022-05-01 14:23:01 +0200

    outputs resources updates

--- core/client.py
+++ core/client.py
@@ -5,12 +5,14 @@ from boto3.dynamodb.conditions                  import Key
 from conn                                       import DBConnect
 
 
+db_data    = DBConnect().db_details()
+dynamodb   = db_data['db_name']
+table_name = db_data['table_name']
+
+
 def create_client(event, context):
 
     client_i   = json.loads(event['body'])
-    db_data    = DBConnect().db_details()
-    dynamodb   = db_data['db_name']
-    table_name = db_data['table_name']
     table      = dynamodb.Table(table_name)
     response   = table.put_item(TableName = table_name, Item = client_i)
     print(response)
@@ -26,9 +28,6 @@ def create_client(event, context):
 
 def get_client(event, context):
 
-    db_data    = DBConnect().db_details()
-    dynamodb   = db_data['db_name']
-    table_name = db_data['table_name']
     table      = dynamodb.Table(table_name)
     client_id  = int(event['pathParameters']['id'])
     response   = table.query(KeyConditionExpression=Key('id').eq(client_id))
